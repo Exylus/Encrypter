64468a3ba567dd170b5209c0181bc7f286f348fb
Exylus <thomy.erwan@gmail.com>
2017-05-18 15:42:06 +0200

    Update

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(526, 495);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(607, 495);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(29, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(653, 477);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(382, 497);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Security Level:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(465, 495);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(37, 20);
+            this.numericUpDown1.TabIndex = 5;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(767, 552);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()  
+        {   
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            decimal security = numericUpDown1.Value;
+            int i = 0;
+            while (i < security)
+            {
+                textBox1.Text = EncryptDecrypt.Encrypt(textBox1.Text);
+                i = i + 1;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            decimal security = numericUpDown1.Value;
+            int i = 0;
+            while (i < security)
+            {
+                textBox1.Text = EncryptDecrypt.Decrypt(textBox1.Text);  
+                i = i + 1;
+            }
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            if (numericUpDown1.Value < 0)
+            {
+                numericUpDown1.Value = 1;
+            }
+            if (numericUpDown1.Value > 5)
+            {
+                if(numericUpDown1.Value <= 7)
+                {
+                    MessageBox.Show("Using a security level above 5 REQUIRES a large amount of memory or errors will occur!");
+                }
+                
+            }
+        }
+    }
+    #region Class
+    #region EncryptDecryt
+    public class EncryptDecrypt
+    {
+        public EncryptDecrypt()
+        {
+        }
+
+        public static string Encrypt(string text_to_Encrypt)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            byte[] bytes = Encoding.ASCII.GetBytes(text_to_Encrypt);
+            checked
+            {
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    byte value = bytes[i];
+                    stringBuilder.Append(Convert.ToString(value, 2).PadLeft(8, '0'));
+                    stringBuilder.Append(" ");
+                }
+                string text = stringBuilder.ToString().Substring(0, stringBuilder.ToString().Length - 1);
+                string text_to_convert = text;
+                return EncryptDecrypt.converttoline(text_to_convert);
+            }
+        }
+
+        public static string Decrypt(string Text_to_Decrypt)
+        {
+            string input = EncryptDecrypt.converttoline(Text_to_Decrypt);
+            string text = Regex.Replace(input, "[^01]", "");
+            checked
+            {
+                byte[] array = new byte[(int)Math.Round(unchecked((double)text.Length / 8.0 - 1.0)) + 1];
+                int arg_4F_0 = 0;
+                int num = array.Length - 1;
+                int num2 = arg_4F_0;
+                while (true)
+                {
+                    int arg_77_0 = num2;
+                    int num3 = num;
+                    if (arg_77_0 > num3)
+                    {
+                        break;
+                    }
+                    array[num2] = Convert.ToByte(text.Substring(num2 * 8, 8), 2);
+                    num2++;
+                }
+                return Encoding.ASCII.GetString(array);
+            }
+        }
+
+        private static string converttoline(string text_to_convert)
+        {
+            bool flag = text_to_convert.Contains("|");
+            string text;
+            if (flag)
+            {
+                text = text_to_convert.Replace(" || ", "0");
+                text = text.Replace(" | ", "1");
+            }
+            else
+            {
+                text = text_to_convert.Replace("0", " || ");
+                text = text.Replace("1", " | ");
+            }
+            return text;
+        }
+    }
+    #endregion
+    #endregion
+
+}
+
